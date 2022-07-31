57552102c5cc55e1620200737f968e6ad8f045a8
habiba156 <69766371+habiba156@users.noreply.github.com>
2022-07-31 03:35:51 -0700

    cw2

---
+++ cw2/Assets/cw2.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cw2 : MonoBehaviour
+{
+
+
+    string heroname = "memo";
+    int herohieght = 155;
+    int heroage = 20;
+    string herosuperpower = "lovely";
+
+    string villianname = "drakula";
+    int villianhieght = 164;
+    int villiainage = 16;
+    string villiansuperpower = "evil";
+
+   int agedifference = 20 - 16;
+
+
+    print("I am" + heroname + "I am" + herohieght + "feet tall." + "I am only" + heroage + "years old." +"My superpower is" + herosuperpower);
+    print("I am" + villianname + "I am" + villianhieght + "feet tall." + "I am only" + vilianage + "years old." + "My superpower is" + villiansuperpower);
+    print("Between" + heroname + "and" + villianname + "there is a" + agedifference + "year age difference");
+
+
+
+
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+}
